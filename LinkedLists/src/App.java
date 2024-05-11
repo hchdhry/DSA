5ee1c5642b7fb6032a92361477cd178d706cf6e3
hassan <hchoudhury20@hotmail.com>
2024-05-11 16:20:51 +0100

    compare array list and linked list

--- LinkedLists/src/App.java
+++ LinkedLists/src/App.java
@@ -1,8 +1,33 @@
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
         LinkedList<Integer> LinkedList = new LinkedList<Integer>();
+        ArrayList<Integer> ArrayList = new ArrayList<Integer>();
+
+        long starTime;
+        long endTime;
+        long elapsedTime;
+
+
+       
+        for(int i=0;i<1000000;i++)
+        {
+             LinkedList.add(i);
+             ArrayList.add(i);
+        }
+        starTime = System.nanoTime();
+        LinkedList.remove(500000);
+        endTime = System.nanoTime();
+        elapsedTime = endTime - starTime;
+        System.out.println("elapsed time for linked list: " + elapsedTime);
+
+        starTime = System.nanoTime();
+        ArrayList.remove(500000);
+        endTime = System.nanoTime();
+        elapsedTime = endTime - starTime;
+        System.out.println("elapsed time for array list : " + elapsedTime);
         
     }
 }
