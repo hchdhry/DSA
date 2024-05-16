fb11b651a103861d6da2a96e75663105074fdee5
hassan <hchoudhury20@hotmail.com>
2024-05-16 21:59:57 +0100

    start hashtable exercise

---
+++ HashTable/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Hashtable;
+
+import javax.print.DocFlavor.STRING;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Hashtable<Integer,String> table = new Hashtable<>(10);
+        
+    }
+}
