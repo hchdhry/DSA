72c732e9d4f81dd6414bb77239ee34462929f987
hassan <hchoudhury20@hotmail.com>
2024-05-16 12:43:59 +0100

    recursivley apply exponents

--- Recursion/src/App.java
+++ Recursion/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(power(3, 4));
+        
     }
+
+    public static int power( int num, int Exponent)
+    {
+       if (Exponent<1) return 1;
+    
+        return num* power(num,Exponent-1);
+        
+
+    }
+    
 }
